a4116d12015cadf25db7f9fa17be19b10fc46f1a
IOhannes m zmölnig <zmoelnig@umlaeute.mur.at>
2011-03-17 08:58:51 +0100

    raw video player
    so we don't have to convert to a compressed file first

---
+++ playraw.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+## usage:
+# $0 <infile> <outfile>
+
+#settings are hardcoded here, so we don't need to care
+WIDTH=320
+HEIGHT=240
+FRAMERATE="(fraction)25/1"
+
+
+INFILE=$1
+
+echo "playing raw ${INFILE}"
+
+
+if [ -e "${INFILE}" ]; then
+ :
+else
+ echo "no valid input file given: \"${INFILE}\"" 1>&2
+ exit 1
+fi
+
+FRAMESIZE=$((WIDTH * HEIGHT * 4))
+if [ "${FRAMESIZE}" -lt 4 ]; then
+ echo "refusing to read videos with framesize < \"${FRAMESIZE}\"" 1>&2
+ exit 1
+fi
+
+gst-launch -v filesrc location="${INFILE}" blocksize=${FRAMESIZE} ! \
+    "video/x-raw-rgb, bpp=(int)32, endianness=(int)4321, depth=(int)32, red_mask=(int)16711680, green_mask=(int)65280, blue_mask=(int)255, alpha_mask=(int)-16777216, width=(int)${WIDTH}, height=(int)${HEIGHT}, framerate=${FRAMERATE}, pixel-aspect-ratio=(fraction)1/1" !\
+    ffmpegcolorspace ! \
+    videorate ! \
+    xvimagesink
+
+
+
