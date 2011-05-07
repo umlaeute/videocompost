7bcee62af14af7ee0d16fb847544f6c70a8370ee
Jogi Hofmüller <j.hofmueller@student.tugraz.at>
2011-05-07 18:29:09 +0200

    added check for runfile upon startup

--- bin/composter.py
+++ bin/composter.py
@@ -35,13 +35,17 @@ def mainLoop ():
   cycletime = 30
   signal.signal (signal.SIGINT, interrupthandler)
   signal.signal (signal.SIGHUP, interrupthandler)
+  if os.path.isfile (pidfilename):
+    writelog ("[composter]:  stale (?) pidfile found.  Exiting.")
+    return 1;
   pidfile = open (pidfilename, "w")
   pidfile.write ("{0}".format (os.getpid ()))
   pidfile.close ()
+  writelog ("[composter]:  started with pid {0}".format (os.getpid ()))
   while True:
     botlist = BotList ()
     numbots = len (botlist.bots)
-    writelog ("[composter]: started cycle with {0} seconds per bot".format (cycletime / numbots))
+    writelog ("[composter]:  started cycle with {0} seconds per bot".format (cycletime / numbots))
     for b in botlist.getList ():
       try:
         bot = __import__ (b)
@@ -53,21 +57,23 @@ def mainLoop ():
           signal.signal (signal.SIGALRM, alarmhandler)
           signal.alarm (cycletime / numbots)
           try:
-            writelog ("[composter]: waiting for {0}[.py] with pid {1}".format (b, pid))
+            writelog ("[composter]:  waiting for {0}[.py] with pid {1}".format (b, pid))
             rv = os.waitpid (pid, 0)
-            writelog ("[composter]: {0}[.py] with pid {1} returned {2}".format (b, pid, rv))
+            writelog ("[composter]:  {0}[.py] with pid {1} returned {2}".format (b, pid, rv))
           except VCTimeout:
             os.kill (pid, signal.SIGHUP)
             rv = os.waitpid (pid, 0)
-            writelog ("[composter]: {0} with pid {1} returned {2} on SIGHUP after timeout".format (b, pid, rv))
+            writelog ("[composter]:  {0} with pid {1} returned {2} on SIGHUP after timeout".format (b, pid, rv))
       except ImportError:
-        writelog ("[composter]: Error importing {0}[.py]".format (b))
+        writelog ("[composter]:  Error importing {0}[.py]".format (b))
       except VCInterrupt:
         os.kill (pid, signal.SIGHUP)
         rv = os.waitpid (pid, 0)
-        writelog ("[composter]: {0} with pid {1} returned {2} on SIGHUP after interrupt".format (b, pid, rv))
+        writelog ("[composter]:  {0} with pid {1} returned {2} on SIGHUP after interrupt".format (b, pid, rv))
+        os.remove (pidfilename)
         return 0
-  writelog ("[composter]: Don't know how I got here ...")
+  writelog ("[composter]:  Don't know how I got here ...")
+  os.remove (pidfilename)
   return 0
 
 if __name__ == "__main__":
