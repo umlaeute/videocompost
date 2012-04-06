06af7d1934b2882279e9584e448486709875c422
IOhannes m zmölnig <zmoelnig@umlaeute.mur.at>
2012-04-06 18:28:25 +0200

    raw player plays!

--- bin/playraw.py
+++ bin/playraw.py
@@ -7,69 +7,102 @@ pygst.require("0.10")
 import gst
 from Compost import Compost
 
+WIDTH=320
+HEIGHT=240
+FRAMERATE="(fraction)25/1"
+
 class GTK_Main:
 	
 	def __init__(self, directoryname="/tmp"):
+		self.blocksize = WIDTH * HEIGHT * 4
+
 		self.dir = directoryname
 		self.compost_init()
-		print "hellow world\n"
-		print self.compost
 		## liste mit verfuegbaren chunks
 		# self.compost._chunkss
 		self.chunkindex=0
 		window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		window.set_title("Video-Player")
 		window.set_default_size(500, 400)
-#		window.fullscreen()
+		window.fullscreen()
 		window.connect("destroy", gtk.main_quit, "WM destroy")
 		vbox = gtk.VBox()
 		window.add(vbox)
 		self.movie_window = gtk.DrawingArea()
 		vbox.add(self.movie_window)
 		window.show_all()
+
+		self.pipeline = gst.Pipeline()
+
+		# open a file		
+		self.fileplayer = gst.element_factory_make("filesrc")
+		self.pipeline.add(self.fileplayer)
+
+		self.fileplayer.set_property("blocksize", self.blocksize)
+
+		# force caps
+		self.capsstring = "video/x-raw-rgb, bpp=(int)32, endianness=(int)4321, depth=(int)32, red_mask=(int)16711680, green_mask=(int)65280, blue_mask=(int)255, alpha_mask=(int)-16777216, width=(int)%d, height=(int)%d, framerate=%s, pixel-aspect-ratio=(fraction)1/1" % (WIDTH, HEIGHT, FRAMERATE)
+		caps = gst.Caps(self.capsstring)
+		self.capsfilter = gst.element_factory_make("capsfilter")
+		self.pipeline.add(self.capsfilter)
+		self.capsfilter.set_property("caps", caps)
+
+		# colorspace converter
+		self.colorspace = gst.element_factory_make("ffmpegcolorspace")
+		self.pipeline.add(self.colorspace)
+
+		# videorate converter
+		self.videorate = gst.element_factory_make("videorate")
+		self.pipeline.add(self.videorate)
 		
-		self.player = gst.element_factory_make("playbin2", "player")
+		# output window
+		self.sink = gst.element_factory_make("xvimagesink")
+		self.pipeline.add(self.sink)
 
+		self.sink.set_xwindow_id(self.movie_window.window.xid)
 
-		bus = self.player.get_bus()
+		gst.element_link_many(self.fileplayer, self.capsfilter, self.colorspace, self.videorate, self.sink)
+		
+		# bus magic
+		bus = self.pipeline.get_bus()
 		bus.add_signal_watch()
 		bus.enable_sync_message_emission()
 		bus.connect("message", self.on_message)
 		bus.connect("sync-message::element", self.on_sync_message)
 
-		self.player.set_property("uri", "file:///tmp/anim-1.mov")
-		self.player.set_state(gst.STATE_PLAYING)
+		self.fileplayer.set_property("location", self.get_next_file())
+		self.pipeline.set_state(gst.STATE_PLAYING)
 		
 	def start_stop(self, w):
 		if self.button.get_label() == "Start":
 			filepath = self.entry.get_text()
 			if os.path.isfile(filepath):
 				self.button.set_label("Stop")
-				self.player.set_property("uri", "file://" + filepath)
-				self.player.set_state(gst.STATE_PLAYING)
+				self.fileplayer.set_property("location", filepath)
+				self.pipeline.set_state(gst.STATE_PLAYING)
 		else:
-			self.player.set_state(gst.STATE_NULL)
+			self.pipeline.set_state(gst.STATE_NULL)
 			self.button.set_label("Start")
 						
 	def on_message(self, bus, message):
 		t = message.type
 		if t == gst.MESSAGE_EOS:
-			self.player.set_state(gst.STATE_NULL)
-			self.player.set_property("uri", "file://" + self.get_next_file())
-			self.player.set_state(gst.STATE_PLAYING)
-			#self.player.set_state(gst.STATE_NULL)
+			self.pipeline.set_state(gst.STATE_NULL)
+			self.fileplayer.set_property("location", self.get_next_file())
+			self.pipeline.set_state(gst.STATE_PLAYING)
+			#self.pipeline.set_state(gst.STATE_NULL)
 			#self.button.set_label("Start")
 		elif t == gst.MESSAGE_ERROR:
-			self.player.set_state(gst.STATE_NULL)
+			self.pipeline.set_state(gst.STATE_NULL)
 			err, debug = message.parse_error()
 			print "Error: %s" % err, debug
 #			self.button.set_label("Start")
 
 	def get_next_file(self):
-		print self.compost._chunks
-		print "======================"
 		chunk = self.get_next_chunk()
-		return chunk._filename
+		if chunk != None:
+			return chunk._filename
+		return None
 
 	def get_next_chunk(self):
 		if self.chunkindex < len(self.compost._chunks):
