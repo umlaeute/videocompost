ac587983834cc5fee56ff190fd8a0f0db6fa7007
Jogi Hofmüller <jogi@mur.at>
2012-04-18 12:26:21 +0200

    added deletechunks.py to repository

---
+++ bin/deletechunks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import time
+from Compost import Compost
+
+compost = Compost ()
+compost.delete ()
+# for chunk in compost.chunks:
+#   print compost.chunks.index (chunk)
+#   compost.chunks.remove (chunk)
+# vim: tw=0 ts=2 expandtab
+# EOF
