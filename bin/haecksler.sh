f7c2c125e8c88718a6c76efb2f315b842c352354
Jogi Hofmüller <jogi@mur.at>
2012-04-19 15:42:44 +0200

    removing old infile.raw now

--- bin/haecksler.sh
+++ bin/haecksler.sh
@@ -8,6 +8,7 @@ basedir="/home/${user}"
 indir="${basedir}/incoming"
 logger="${basedir}/bin/VCLogger.py"
 lockfile="${basedir}/run/haecksler.lock"
+rawvideo="${basedir}/infile.raw"
 
 # exit if a lockfile is present
 if [ -f ${lockfile} ]
@@ -23,19 +24,25 @@ cd ${basedir}
 # add videos sorted by upload time
 for video in $(ls -t ${indir})
 do
-  infile="${indir}/${video}"
+  oggvideo="${indir}/${video}"
+  
+  # remove old rawvideo if there is one
+  if [ -f ${rawvideo} ]
+  then
+    rm -f ${rawvideo}
+  fi
 
-  if [ -f ${infile} ]
+  if [ -f ${oggvideo} ]
   then
     # convert it to .raw format
     ${logger} "[haecksler]: importing ${video}"
-    ./bin/video2raw.sh ${infile} infile.raw
+    ./bin/video2raw.sh ${oggvideo} ${rawvideo}
     
     # cut it to pieces and add it to compost
     ./bin/cutvideo.py
     
     # remove video from incoming
-    rm -f ${infile}
+    rm -f ${oggvideo}
     ${logger} "[haecksler]: finished importing ${video}"
   fi
 done
