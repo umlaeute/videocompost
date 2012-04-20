950ad622d3d2b15c97a38cb2ffa58a5eb34e9b00
Jogi Hofmüller <jogi@mur.at>
2012-04-20 17:48:35 +0200

    improved log output

--- bin/cutvideo.py
+++ bin/cutvideo.py
@@ -17,7 +17,8 @@ from VCLogger import writelog
 if os.path.isfile (infilename):
   infile = open (infilename, "r")
 else:
-  print "%s not found" % infilename
+  writelog ("[cutvideo]: {0} not found".format (infilename))
+  print "[cutvideo]: {0} not found".format (infilename)
   sys.exit (1)
 
 # pixels_per_frame * bytes_per_pixel * frames
@@ -32,7 +33,6 @@ while True:
   infile.seek (chunksize * chunknum)
   chunk = infile.read (chunksize)
   if not chunk:
-    # print "Done"
     break
   hash = hashlib.md5 (str (time.time ()))
   chunkfilename = os.path.join (compostdir, "%s.raw" % hash.hexdigest ())
@@ -40,13 +40,11 @@ while True:
   chunkfile.write (chunk)
   chunkfile.close ()
   compost.addChunk (chunkfilename)
-  # print "wrote chunk #%4d with name %s" % (chunknum, chunkfilename)
   chunknum += 1
 
-writelog ("[cutvideo] added {0} chunks from {1} to compost.".format (chunknum, infilename))
 os.remove (infilename)
 compost.save ()
-# compost.show ()
+writelog ("[cutvideo]: successfully added {0} chunks from {1} to compost.".format (chunknum, infilename))
 
 # vim: tw=0 ts=2 expandtab
 # EOF
