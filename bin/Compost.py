91b5509ef57be8344d37001e2ef12f8ffe139dfd
Jogi Hofmüller <jogi@mur.at>
2012-04-22 17:06:20 +0200

    closing file after reading from it

--- bin/Compost.py
+++ bin/Compost.py
@@ -64,6 +64,7 @@ class Compost:
     self.lockCompost ()
     picklefile = open (self._filename, "r")
     self._chunks = pickle.load (picklefile)
+    picklefile.close ()
     self.unlockCompost ()
 
   def lockCompost (self):
