e5ed92484e12d4af8bfc76603a8dc4e8db2fec24
Jogi Hofmüller <jogi@mur.at>
2012-04-23 14:41:50 +0200

    small changes

--- bin/BotList.py
+++ bin/BotList.py
@@ -9,24 +9,24 @@ from vcconfig import *
 class BotList:
 
   def __init__ (self):
-    self.picklefilename = os.path.join (configdir, "botlist.pck")
+    self.picklefilename = os.path.join (configdir, 'botlist.pck')
     if os.path.isfile (self.picklefilename):
       self.loadList ()
     else:
       self.bots = []
 
   def saveList (self):
-    picklefile = open (self.picklefilename, "w")
-    fcntl.flock (picklefile, fcntl.LOCK_EX)
+    picklefile = open (self.picklefilename, 'w')
+    fcntl.lockf (picklefile, fcntl.LOCK_EX)
     pickle.dump (self.bots, picklefile)
-    fcntl.flock (picklefile, fcntl.LOCK_UN)
+    fcntl.lockf (picklefile, fcntl.LOCK_UN)
     picklefile.close ()
 
   def loadList (self):
-    picklefile = open (self.picklefilename, "r")
-    fcntl.flock (picklefile, fcntl.LOCK_EX)
+    picklefile = open (self.picklefilename, 'r+')
+    fcntl.lockf (picklefile, fcntl.LOCK_EX)
     self.bots = pickle.load (picklefile)
-    fcntl.flock (picklefile, fcntl.LOCK_UN)
+    fcntl.lockf (picklefile, fcntl.LOCK_UN)
 
   def getList (self):
     return self.bots
@@ -40,7 +40,7 @@ class BotList:
           return
       self.bots.append (newbot)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   pass
 
 # vim: tw=0 ts=2 expandtab
